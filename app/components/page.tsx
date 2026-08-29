4fdecd8e5fc847b98d4bcbe048331921c6c05bb2
Ratan Prjapati <rpusgroupmail@gmail.com>
2026-08-29 09:03:23 +0530

    hero added

--- app/components/page.tsx
+++ app/components/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { ArrowLeft, Layers, Box, Columns, Footprints, Shield, Zap, Sparkles, ArrowRight, CheckCircle2 } from "lucide-react";
+import AdBanner from "@/components/AdBanner";
 
 
 const componentsList = [
